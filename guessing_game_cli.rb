7c4efd12517013d9fa1c6d69c3c2c82948e6614c
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-09 22:07:44 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,21 +1,45 @@
-# # Code your solution here!
+# # # Code your solution here!
 require 'pry'
 
-# def run_guessing_game
-#   random = rand(6) + 1
+# # def run_guessing_game
+# #   random = rand(6) + 1
   
-#   puts "Guess a number between 1 and 6"
+# #   puts "Guess a number between 1 and 6"
   
-#   input = gets.chomp
+# #   input = gets.chomp
 
+# #   if input == "exit"
+# #     puts "Goodbye!"
+# #   else
+# #     if random.to_s == input
+# #       puts "You guessed the correct number!"
+# #     else
+# #       puts "Sorry! The computer guessed #{random}."
+# #     end
+# #   end
+# # end
+
+
+
+
+
+
+
+
+
+# def run_guessing_game
+#   num = rand(6) + 1
+#   binding.pry
+#   puts "Pick a number between 1 and 6"
+#   input = gets.chomp
+  
 #   if input == "exit"
 #     puts "Goodbye!"
-#   else
-#     if random.to_s == input
-#       puts "You guessed the correct number!"
-#     else
-#       puts "Sorry! The computer guessed #{random}."
-#     end
+#   elsif input == num.to_s
+#     puts "You guessed the correct number!"
+    
+#   elsif input != num && input != "exit"
+#     puts "Sorry! The computer guessed #{num}."
 #   end
 # end
 
@@ -27,21 +51,31 @@ require 'pry'
 
 
 
+
+
 def run_guessing_game
-  num = rand(6) + 1
-  binding.pry
-  puts "Pick a number between 1 and 6"
-  input = gets.chomp
+  random_num = rand(6) + 1
+  input = gets.strip
   
-  if input == "exit"
+  
+  
+  
+  if input == 'exit'
+  
     puts "Goodbye!"
-  elsif input == num.to_s
+  elsif input != random_num.to_s
+    puts "Sorry! The computer guessed 6."
+  elsif input == random_num.to_s
     puts "You guessed the correct number!"
-    
-  elsif input != num && input != "exit"
-    puts "Sorry! The computer guessed #{num}."
   end
 end
+
+
+
+
+
+
+
 
 
 
